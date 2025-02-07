7ed1c1da2142d590354d64c99e08d340d11e51bf
EDSON GOMES ALVES DE ARAUJO <edsongada@policiamilitar.sp.gov.br>
2025-02-07 17:05:31 -0300

    Implantando Arquitetura Limpa

---
+++ DevFreela.Core/Exceptions/ProjectAlreadyStaredException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Exceptions
+{
+    public class ProjectAlreadyStaredException : Exception
+    {
+        public ProjectAlreadyStaredException() : base("Project is Already in Started satus")
+        {
+            
+        }
+
+    }
+}
